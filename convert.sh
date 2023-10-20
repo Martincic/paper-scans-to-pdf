a76ee0ac483f2a6bd7bc39a718e708a004319aba
Martincic <tomas.martincic@q.agency>
2023-10-20 18:53:06 +0200

    v1 pipeline

--- convert.sh
+++ convert.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
-# Rotate all images upright
+# Create or clean working directories
 echo $(date +%H:%M:%S)
-echo -e "\e[32m\e[1mRotating images\e[0m"
+echo "Creating/cleaning work directories"
 
+rm -rf output
+rm -rf rotate
+
+mkdir output
+mkdir rotate
+
+# Rotate all images upright
+echo -e "\e[32m\e[1mRotating images\e[0m"
 counter=0
 
 for filename in ./data/*; do
-	convert ./data/*.jpg -auto-orient -rotate "90>" rotate/${counter}.jpg
-	counter=$((counter+1))
-	echo "${counter}"
+	extension="${filename##*.}"
+	output="rotate/${counter}.${extension}"
+
+	echo $(date +%H:%M:%S)
+	echo "Converting ${filename}"
+	echo "Output: ${output}\n\n"
+
+	convert "${filename}" -auto-orient -rotate "90>" $output
+	counter=$((counter + 1))
 done
-
-echo 'exiting'
-exit
-
-echo "Rotating *.JPGs"
-convert ./data/*.jpg -auto-orient -rotate "90>" rotate/outputA.jpg
-
-echo $(date +%H:%M:%S)
-echo "Rotating *.JPEGs"
-convert ./data/*.jpeg -auto-orient -rotate "90>" rotate/outputC.jpeg
-
-echo $(date +%H:%M:%S)
-echo "Rotating *.PNGs"
-convert ./data/*.png -auto-orient -rotate "90>" rotate/outputB.png
 
 
 # Convert images to grayscale, fix contrast and shaddows
 echo -e "\e[32m\e[1mCleaning up colors and contrast\e[0m"
 echo $(date +%H:%M:%S)
-echo "Cleaning jpgs.."
-convert rotate/*.jpg -colorspace Gray -contrast-stretch 0x10% output/outA.jpg
+counter=0
+for filename in ./data/*; do
+        extension="${filename##*.}"
+        output="output/${counter}.${extension}"
 
-echo $(date +%H:%M:%S)
-echo "Cleaning jpegs.."
-convert rotate/*.jpeg -colorspace Gray -contrast-stretch 0x3% -normalize output/outB.jpeg
+	echo $(date +%H:%M:%S)
+	echo "Converting ${filename}"
+        echo "Output: ${output}\n\n"
 
-echo $(date +%H:%M:%S)
-echo "Cleaning pngs.."
-convert rotate/*.png -colorspace Gray -contrast-stretch 0x1% -normalize output/outC.png
-
+	convert "${filename}" -colorspace Gray -contrast-stretch 0x10% $output
+        counter=$((counter + 1))
+done
 
 # Images to PDF file
 echo -e "\e[32m\e[1mDone! Compiling PDF output...\e[0m"
